e8671427e9ddc140cfa291cd34c1139a59c13ad3
Mischa Spiegelmock <revmischa@cpan.org>
2016-02-06 12:08:34 -0800

    bump version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ Read the README for more information: https://github.com/revmischa/udo/blob/mast
 
 setup(
     name='udo',
-    version='4.1.0',
+    version='4.2.0',
     url='http://github.com/revmischa/udo',
     license='Anyone But RMS',
     author='Mischa Spiegelmock',
